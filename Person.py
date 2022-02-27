e0f8bb586f0db849cfeee62649722da391fe2ceb
PrimeGod <32197961+PrimeGod@users.noreply.github.com>
2022-02-27 18:02:27 -0500

    Adding part1 of the test

---
+++ Person.py
@@ -0,0 +1,25 @@
+'''
+Example of Person Class for technical test
+By Philippe Langevin
+'''
+
+class Person(object):
+    age = -1   
+
+    def __init__(self, initialAge):
+        if self.age >= 0 and type(self.age) == int:
+            self.age = initialAge
+        else:
+            print("The input parameter 'age' of the constructor" +
+            "isn't valid")
+    
+    def what_am_i(self):
+        if self.age < 13:
+            print("You are young")
+        elif self.age >= 13 and self.age < 18:
+            print("You are a teenager")
+        else:
+            print("You are an adult")
+
+    def year_passes(self):
+        self.age += 1
